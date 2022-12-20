e8acc740e54be74bd2ec1c1e336fc2a111032437
AbhishaShah <abhisha5993@gmail.com>
2022-12-20 15:06:04 -0500

    Started Typescript intro

---
+++ 01-Intro.ts
@@ -0,0 +1,7 @@
+let greetings:string = "Hello World!";
+
+greetings.toUpperCase(); // Smartly Only give string methods.
+
+// greetings = 10; -> Error: Type 'number' is not assignable to type 'string'.ts(2322)
+
+export {} // Temporary to remove ts error.

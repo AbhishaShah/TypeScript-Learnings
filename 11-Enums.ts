23a138ababcde77b08ace5e1ca77c0c4cfa52507
AbhishaShah <abhisha5993@gmail.com>
2023-01-12 12:27:30 -0500

    Mixed type of enums

--- 11-Enums.ts
+++ 11-Enums.ts
@@ -1,12 +1,20 @@
 //enum type: useful to define a set of named constants
 
+//numeric enums: All of the following members are auto-incremented from that point on. In other words, menuItems.SERVICE has the value 2, ABOUT has 3.
 const enum menuItems {
+    PRODUCTS = 1,
+    SERVICE,
+    ABOUT
+}
+
+//string enums
+const enum menuItems1 {
     PRODUCTS = 'Products',
     SERVICE = 'Service',
     ABOUT = 'About',
 }
 
-const menubar = menuItems.PRODUCTS;
+const menubar = menuItems1.PRODUCTS;
 
 //const menubar1 = menuItems.PRODUCTSe // TS Error: Property 'PRODUCTSe' does not exist on type 'typeof menuItems'.
 
